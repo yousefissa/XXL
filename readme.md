08c8b0c360bb51f731b18cc332526cf057b65921
yomo710 <yomo710@gmail.com>
2017-03-28 01:26:02 -0500

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Vote for XXXTentacion for the last 2017 XXL Freshman class spot.
+
+note: it's 1:25 am and i havent slept much in the past days. idek why i made this.
---
+++ xxxtent.py
@@ -0,0 +1,20 @@
+import requests
+url = 'http://freshman.xxlmag.com/10th-spot/?2017'
+
+headers = {'User-Agent':
+           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+
+
+def main(limit):
+    for i in range(1, limit):
+        payload = {
+            'action':'media_poll_vote',
+            'poll_id': 9701059,
+            'post_id':232,
+            'answer_id':'44359115'
+        }
+        resp = requests.post(url, data=payload, headers=headers)
+        print('{}/{} votes.'.format(i, limit))
+
+if __name__ == "__main__":
+    main(10000)
